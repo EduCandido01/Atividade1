760282380c79af905ddcd5e001b7eb1314cf88fb
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-23 11:19:51 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("escreva o número");
+        int num = Convert.ToInt32 (Console.ReadLine());
+
+        bool resultado = num < 10 && num > 20;
+
+        if (resultado)
+        {
+            Console.WriteLine("fora do intervalo");
+        }
+
+        else
+        {
+            Console.WriteLine("debtro do intervalo");
+        }
+    }
+}
+
