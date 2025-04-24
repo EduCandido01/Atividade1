2a94d444e863cfd54b517b8a8afd9282ace45844
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-24 09:02:55 -0300

    atv3

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("escreva o número");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("escreva o número");
+        string verdade = Console.ReadLine();
+
+        bool resultado = num >= 18 && verdade == "sim";
+
+        if (resultado)
+        {
+            Console.WriteLine("pode dirigir");
+        }
+
+        else
+        {
+            Console.WriteLine("não pode dirigir");
+        }
+    }
+}
